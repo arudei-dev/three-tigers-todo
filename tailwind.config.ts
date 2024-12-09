b3108180684b0eb8645514d858df0461f7b977e5
Aldey Wahyu Putra <al.arudei01@gmail.com>
2024-12-09 16:35:52 +0700

    feat: setup jest + adjust tailwind (#2)
    * feat: setup jest
    
    * feat: setup tailwind theme + new paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,24 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/entities/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "chip-default": "#E8E2E2",
+        "chip-active": "#D2E3DF",
+      },
+      textColor: {
+        "chip-default": "#4D4D52",
+        "chip-active": "#004736",
+      },
+      boxShadow: {
+        appBar: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
       },
     },
   },
