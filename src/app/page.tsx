08f00281a00e25ccc2f8750adff93c484211cf7f
Aldey Wahyu Putra <al.arudei01@gmail.com>
2024-12-09 17:07:04 +0700

    fix: move page due to incompatibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import { useState } from "react";
+
+import { TodoItem } from "@/entities/todo/api";
 import useGetTodoList from "@/entities/todo/models/todo";
-import { FilterBar } from "@/features/filters/ui";
-import { TodoView } from "@/pages/todo";
+import { FilterBar, FilteredList } from "@/features/filters/ui";
 import { AppBar } from "@/shared/ui";
+
+interface TodoViewProps {
+  data: TodoItem[];
+}
+
+function TodoView(props: TodoViewProps) {
+  const [list, setList] = useState(props.data);
+
+  const handleClickCheckbox = (id: number) => {
+    const newList = [...list];
+    const currIdx = newList.findIndex((item) => item.id === id);
+
+    newList[currIdx].completed = !newList[currIdx].completed;
+
+    setList(newList);
+  };
+
+  return <FilteredList todoList={list} onClickCheckbox={handleClickCheckbox} />;
+}
 
 export default function Home() {
   const todoResponse = useGetTodoList();
